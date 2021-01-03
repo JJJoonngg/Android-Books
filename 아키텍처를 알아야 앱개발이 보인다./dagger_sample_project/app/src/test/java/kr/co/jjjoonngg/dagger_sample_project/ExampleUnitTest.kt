d143fdf0c7ddca9d3355ac28644deb6ed1d5e47a
JJJoonngg <whdtls3878@gmail.com>
2021-01-03 21:07:12 +0900

    reactive vs imperative  programming example

--- 아키텍처를 알아야 앱개발이 보인다./dagger_sample_project/app/src/test/java/kr/co/jjjoonngg/dagger_sample_project/ExampleUnitTest.kt
+++ 아키텍처를 알아야 앱개발이 보인다./dagger_sample_project/app/src/test/java/kr/co/jjjoonngg/dagger_sample_project/ExampleUnitTest.kt
@@ -1,6 +1,7 @@
 package kr.co.jjjoonngg.dagger_sample_project
 
 import dagger.MembersInjector
+import io.reactivex.rxjava3.subjects.PublishSubject
 import kr.co.jjjoonngg.dagger_sample_project.daggerbase.DaggerMyComponent
 import kr.co.jjjoonngg.dagger_sample_project.daggerbase.MyClass
 import kr.co.jjjoonngg.dagger_sample_project.daggerbase.MyComponent
@@ -41,5 +42,54 @@ class ExampleUnitTest {
         injector.injectMembers(myClass)
         str = myClass.str
         println("result = $str")
+    }
+
+    @Test
+    fun imperativeProgramming() {
+        val items = ArrayList<Int>()
+        items.add(1)
+        items.add(2)
+        items.add(3)
+        items.add(4)
+
+        //Print Even Number
+        for (item in items) {
+            if (item % 2 == 0) {
+                println(item)
+            }
+        }
+
+        /*
+        결과
+        2
+        4
+        */
+
+        items.add(5)
+        items.add(6)
+        items.add(7)
+        items.add(8)
+    }
+
+    @Test
+    fun reactiveProgramming() {
+        val items: PublishSubject<Int> = PublishSubject.create()
+        items.onNext(1)
+        items.onNext(2)
+        items.onNext(3)
+        items.onNext(4)
+        //Print Even Number
+        items.filter { item -> item % 2 == 0 }
+            .subscribe(System.out::println)
+
+        items.onNext(5)
+        items.onNext(6)
+        items.onNext(7)
+        items.onNext(8)
+        /*
+        결과
+        6
+        8
+         */
     }
 }
