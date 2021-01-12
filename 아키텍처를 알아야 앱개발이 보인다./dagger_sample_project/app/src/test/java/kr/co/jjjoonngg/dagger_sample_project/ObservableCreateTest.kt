3469256797919d3f9961e8de12497435aee35f11
JJJoonngg <whdtls3878@gmail.com>
2021-01-12 22:25:39 +0900

    Observable - cold, hot, connectable, autoConnect, compositeDisposable

--- 아키텍처를 알아야 앱개발이 보인다./dagger_sample_project/app/src/test/java/kr/co/jjjoonngg/dagger_sample_project/ObservableCreateTest.kt
+++ 아키텍처를 알아야 앱개발이 보인다./dagger_sample_project/app/src/test/java/kr/co/jjjoonngg/dagger_sample_project/ObservableCreateTest.kt
@@ -1,10 +1,13 @@
 package kr.co.jjjoonngg.dagger_sample_project
 
 import io.reactivex.rxjava3.core.*
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.observables.ConnectableObservable
 import org.junit.Test
 import org.reactivestreams.Publisher
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 
 /*
 * Created by JJJoonngg
@@ -156,5 +159,84 @@ class ObservableCreateTest {
         completed1
         completed2
         */
+    }
+
+    @Test
+    fun coldObservable() {
+        Observable.interval(1, TimeUnit.SECONDS).also {
+            it.subscribe { value -> println("#1 : $value") }
+            Thread.sleep(3000)
+            it.subscribe { value -> println("#2 : $value") }
+            Thread.sleep(3000)
+        }
+
+        /*
+        결과
+        #1 : 0
+        #1 : 1
+        #1 : 2
+        #1 : 3
+        #2 : 0
+        #1 : 4
+        #2 : 1
+        #1 : 5
+        #2 : 2
+         */
+    }
+
+    @Test
+    fun connectableObservable() {
+        Observable.interval(1, TimeUnit.SECONDS).publish().also {
+            it.connect()
+            it.subscribe { value -> println("#1 : $value") }
+            Thread.sleep(3000)
+            it.subscribe { value -> println("#2 : $value") }
+            Thread.sleep(3000)
+        }
+        /*
+        결과
+        #1 : 0
+        #1 : 1
+        #1 : 2
+        #1 : 3
+        #2 : 3
+        #1 : 4
+        #2 : 4
+        #1 : 5
+        #2 : 5
+         */
+    }
+
+    @Test
+    fun autoConnect() {
+        Observable.interval(100, TimeUnit.MILLISECONDS).publish().autoConnect(2).also {
+            it.subscribe { value -> println("A : $value") }
+            it.subscribe { value -> println("B : $value") }
+            Thread.sleep(500)
+        }
+        /*
+        결과
+        A : 0
+        B : 0
+        A : 1
+        B : 1
+        A : 2
+        B : 2
+        A : 3
+        B : 3
+        A : 4
+        B : 4
+         */
+    }
+
+    @Test
+    fun compositeDisposable() {
+        val source = Observable.interval(1000, TimeUnit.MILLISECONDS)
+
+        val d1 = source.subscribe(System.out::println)
+        val d2 = source.subscribe(System.out::println)
+        val d3 = source.subscribe(System.out::println)
+        val cd = CompositeDisposable()
+        cd.addAll(d1,d2,d3)
     }
 }
